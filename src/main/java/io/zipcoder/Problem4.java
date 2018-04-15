59455107ed8f2a69f657c5a87f820d1483f7c0af
Brian He <brianhe@zipcoders-MacBook-Pro-56.local>
2018-04-15 15:17:34 -0400

    done

--- src/main/java/io/zipcoder/Problem4.java
+++ src/main/java/io/zipcoder/Problem4.java
@@ -1,4 +1,33 @@
 package io.zipcoder;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem4 {
+
+    public static String canBePalindrome(String input) {
+        String[] split = input.split("");
+        Map<String, Integer> occurrences = new HashMap<>();
+
+        for (String s : split) {
+            if (occurrences.get(s) == null) {
+                occurrences.put(s, 1);
+            } else {
+                occurrences.put(s, occurrences.get(s) + 1);
+            }
+        }
+
+        Integer oddCount = 0;
+        for (Map.Entry<String, Integer> entry : occurrences.entrySet()) {
+            if (entry.getValue() % 2 != 0) {
+                oddCount++;
+            }
+        }
+
+        if (oddCount > 1) {
+            return "NO";
+        }
+        return "YES";
+    }
 }
+
--- src/test/java/io/zipcoder/Problem4Test.java
+++ src/test/java/io/zipcoder/Problem4Test.java
@@ -1,4 +1,43 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import static io.zipcoder.Problem4.canBePalindrome;
+
 public class Problem4Test {
+
+    @Test
+    public void canBePalindromeTest() {
+        String input = "aaabbbb";
+        String expected = "YES";
+
+        String actual = canBePalindrome(input);
+
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void canBePalindromeTest2() {
+        String input = "cdefghmnopqrstuvw";
+        String expected = "NO";
+
+        String actual = canBePalindrome(input);
+
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void canBePalindromeTest3() {
+        String input = "cdcdcdcdeeeef";
+        String expected = "YES";
+
+        String actual = canBePalindrome(input);
+
+        Assert.assertEquals(expected, actual);
+
+    }
+
 }
